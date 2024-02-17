2c6d46351b326f159086927e653ba4220f28449c
dagrv <rust.91@icloud.com>
2024-02-17 23:38:51 +0100

    Initial

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const MarketingPage = () => {
+	return (
+		<div>
+			<Button className="bg-emerald-500">
+				Delete
+			</Button>
+		</div>
+	);
+}
+
+export default MarketingPage;
